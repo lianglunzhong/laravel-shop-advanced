135c4715d6cce2d816326955e2fe929b00ebc18f
vagrant <vagrant@homestead>
2018-08-15 07:26:10 +0000

    用户界面 - 订单详情页

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,10 @@ Route::group([
 		Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 		// 保存订单
 		Route::post('orders', 'OrdersController@store')->name('orders.store');
+		// 订单列表
 		Route::get('orders', 'OrdersController@index')->name('orders.index');
+		// 订单详情
+		Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
 	});
 });
 
