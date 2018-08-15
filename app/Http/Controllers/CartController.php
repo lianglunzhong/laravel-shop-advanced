5fcaec5b61b83f19863c44bf8ccab73da31b7416
vagrant <vagrant@homestead>
2018-08-15 08:03:24 +0000

    封装业务代码

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,38 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Models\ProductSku;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
+    protected $cartService;
+
+    // 利用 Laravel 的自动解析功能注入 CartService 类
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
     public function add(AddCartRequest $request)
     {
-    	$user = $request->user();
     	$skuId = $request->input('sku_id');
     	$amount = $request->input('amount');
 
-    	// 从数据库中查询该商品是否已经在购物车中
-    	if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
-    		// 如果存在则直接叠加商品数量
-    		$cart->update([
-    			'amount' => $cart->amount + $amount,
-    		]);
-    	} else {
-    		$cart = new CartItem(['amount' => $amount]);
-    		// 当更新 belongsTo 关联时，可以使用 associate 方法。此方法将会在子模型中设置外键   (dissociate)
-    		$cart->user()->associate($user);
-    		$cart->productSku()->associate($skuId);
-    		$cart->save();
-    	}
+    	$this->cartService->add($skuId, $amount);
 
     	return [];
     }
 
     public function index(Request $request)
     {
-    	$cartItems = $request->user()
-    					->cartItems()
-    					->with(['productSku.product'])
-    					->get();
+    	$cartItems = $this->cartService->get();
 
         $addresses = $request->user()
                         ->addresses()
@@ -51,7 +45,7 @@ class CartController extends Controller
 
     public function remove(ProductSku $sku, Request $request)
     {
-    	$request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+    	$this->cartService->remove($sku->id);
 
     	return [];
     }
--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -11,72 +11,20 @@ use App\Models\OrderItem;
 use Carbon\Carbon;
 use App\Exceptions\InvalidRequestException;
 use App\Jobs\CloseOrder;
+use App\Services\CartService;
+use App\Services\OrderService;
 
 class OrdersController extends Controller
 {
-    public function store(OrderRequest $request)
+    public function store(OrderRequest $request, OrderService $orderService)
     {
     	$user = $request->user();
-    	// 开启一个数据库事务
-    	$order = \DB::transaction(function() use ($user, $request) {
-    		$address = UserAddress::find($request->address_id);
-    		// 更新此地址的最后使用时间
-    		$address->update(['last_used_at' => Carbon::now()]);
+        $address = UserAddress::find($request->address_id);
+        $remark = $request->remark;
+        $items = $request->items;
 
-    		// 创建一个订单
-    		$order = new Order([
-    			'address' => [ // 将地址信息放入订单中
-    				'address' 			=> $address->full_address,
-    				'zip' 				=> $address->zip,
-    				'contact_name' 		=> $address->contact_name,
-    				'contact_phone' 	=> $address->contact_phone,
-    			],
-    			'remark' => $request->remark,
-    			'total_amount' => 0,
-    		]);
-    		// 订单关联到当前用户
-    		$order->user()->associate($user);
-    		// 写入数据库
-    		$order->save();
-
-    		$totalAmount = 0;
-    		$items = $request->items;
-    		// 遍历用户提交的 SKU
-    		foreach ($items as $data) {
-    			$sku = ProductSku::find($data['sku_id']);
-
-    			// 创建一个 OrderItem 并直接与当前订单关联
-    			// $order->items()->make() 等同于 $item = new OrderItem(); $item->order()->associate($order)
-    			$item = $order->items()->make([
-    				'amount' => $data['amount'],
-                    'price'  => $sku->price,
-    			]);
-    			$item->product()->associate($sku->product_id);
-    			$item->productSku()->associate($sku);
-    			$item->save();
-
-    			$totalAmount += $sku->price * $data['amount'];
-
-    			// 减库存
-    			if ($sku->decreaseStock($data['amount']) <= 0) {
-    				throw new InvalidRequestException('该商品库存不足');
-    			}
-    		}
-
-    		// 更新订单总金额
-    		$order->update(['total_amount' => $totalAmount]);
-
-    		// 将下单的商品从购物车中移除
-    		$skuIds = collect($items)->pluck('sku_id');
-    		$user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
-
-    		return $order;
-    	});
-
-        // 触发延迟任务，关闭未支付的订单
-        // 默认情况下，Laravel 生成的 .env 文件里把队列的驱动设置成了 sync（同步），在同步模式下延迟任务会被立即执行，所以需要先把队列的驱动改成 redis
-        $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
-
+        $order = $orderService->store($user, $address, $remark, $items);
+    	
     	return $order;
     }
 
