b780b77beba311cb3a9c69ca4d508cb2c97db80a
vagrant <vagrant@homestead>
2018-09-28 08:26:07 +0000

    初始化类目模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,6 +15,11 @@ class Product extends Model
     	'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     public function skus()
     {
     	return $this->hasMany(ProductSku::class);
