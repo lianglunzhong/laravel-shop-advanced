6c9977ae60f8f9bb6b610e66127f0229d77900ab
vagrant <vagrant@homestead>
2018-10-31 06:17:48 +0000

    使用 Elasticsearch 重构商品搜索功能

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,65 +6,110 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Category;
 use App\Models\OrderItem;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-    	// 创建一个查询构造器
-    	$builder = Product::query()->where('on_sale', true);
-    	// 判断是否有提交 search 参数，如果有就赋值给 $search 变量
-        // search 参数用来模糊搜索商品
-        if ($search = $request->input('search', '')) {
-        	$like = '%' . $search . '%';
-        	// 模糊搜索商品标题、描述、SKU 标题、 SKU 描述
-        	$builder->where(function ($query) use ($like) {
-        		$query->where('title', 'like', $like)
-        			->orWhere('description', 'like', $like)
-        			->orWhereHas('skus', function ($query) use ($like) {
-        				$query->where('title', 'like', $like)
-        					->orWhere('description', 'like', $like);
-        			});
-        	});
-        }
+        $page    = $request->input('page', 1);
+        $perPage = 16;
 
-        // 如果有传入 category_id 字段，并且在数据库中有对应的类目
-        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
-            // 如果这是一个父类目
-            if ($category->is_directory) {
-                // 则筛选出该父类目下所有子类目的商品
-                $builder->whereHas('category', function ($query) use ($category) {
-                    $query->where('path', 'like', $category->path . $category->id . '-%');
-                });
-            } else {
-                // 如果这不是一个父类目，则直接筛选此类目下的商品
-                $builder->where('category_id', $category->id);
-            }
-        }
+        // 构建查询
+        $params = [
+            'index' => 'products',
+            'type'  => '_doc',
+            'body'  => [
+                'from'  => ($page - 1) * $perPage, // 通过当前页数与每页数量计算偏移值
+                'size'  => $perPage,
+                'query' => [
+                    'bool' => [
+                        'filter' => [
+                            ['term' => ['on_sale' => true]],
+                        ],
+                    ],
+                ],
+            ],
+        ];
 
         // 是否有提交 order 参数，如果有就赋值给 $order 变量
         // order 参数用来控制商品的排序规则
-        if ($order =$request->input('order', '')) {
-        	// 是否以 _asc 或者 _desc 结尾
-        	if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
-        		// 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
-        		if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
-        			// 根据传入的排序值来构成排序参数
-        			$builder->orderBy($m[1], $m[2]);
-        		}
-        	}
+        if ($order = $request->input('order', '')) {
+            // 是否是以 _asc 或者 _desc 结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的开头是这 3 个字符串之一，说明是一个合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根据传入的排序值来构造排序参数
+                    $params['body']['sort'] = [[$m[1] => $m[2]]];
+                }
+            }
         }
 
-        $products = $builder->paginate(16);
+        // 类目筛选
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            if ($category->is_directory) {
+                // 如果是一个福类目，则使用 category_path 来筛选
+                $params['body']['query']['bool']['filter'][] = [
+                    // prefix 前缀查询，与 Mysql 的 like '${path}%' 是等价的
+                    'prefix' => ['category_path' => $category->path.$category->id.'-'],
+                ];
+            } else {
+                // 直接通过 category_id 筛选
+                $params['body']['query']['bool']['filter'][] = [
+                    'term' => ['category_id' => $category->id],
+                ];
+            }
+        }
 
-    	return view('products.index', [
-    		'products' => $products,
-    		'filters' => [
-    			'search' => $search,
-    			'order' => $order,
-    		],
+        // 关键字筛选
+        if ($search = $request->input('search', '')) {
+            // 将搜索词根据空格拆分成数组，并过滤掉空项
+            $keywords = array_filter(explode(' ', $search));
+
+            $params['body']['query']['bool']['must'] = [];
+            // 遍历搜索词数组，分别添加到 must 查询中
+            foreach ($keywords as $keyword) {
+                $params['body']['query']['bool']['must'][] = [
+                    'multi_match' => [
+                        'query' => $keyword,
+                        'fields' => [
+                            'title^3',
+                            'long_title^2',
+                            'category^2', // 类目名称
+                            'description',
+                            'skus_title',
+                            'skus_description',
+                            'properties_value',
+                        ],
+                    ],
+                ];
+            }
+        }
+
+        $result = app('es')->search($params);
+
+        // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
+        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        // 通过 whereIn 方法从数据库中读取商品数据
+        $products = Product::query()
+            // $productIds 是按查询条件查询到的正确的排序的结果，但是用 whereIn 时，不会按照 ID 的顺序去查询
+            ->whereIn('id', $productIds)
+            // orderByRaw 可以让我们用原生的 SQL 来给查询结果排序
+            ->orderByRaw(sprintf("FIND_IN_SET(id, '%s')", join(',', $productIds)))
+            ->get();
+        // 返回一个 LengthAwarePaginator 对象
+        $pager = new LengthAwarePaginator($products, $result['hits']['total'], $perPage, $page, [
+            'path' => route('products.index', false), // 手动构建分页的 url
+        ]);
+
+        return view('products.index', [
+            'products' => $pager,
+            'filters'  => [
+                'search' => '',
+                'order'  => $order,
+            ],
             'category' => $category ?? null,
-    	]);
+        ]);
     }
 
     public function show(Product $product, Request $request)
